71d7b5408c504e0863875b8560d83952055f0686
oreillyp <patrickoreilly175@gmail.com>
2022-01-27 17:43:30 +0000

    initial commit

---
+++ code/data.py
@@ -0,0 +1,176 @@
+import numpy as np
+from numpy import pi
+
+################################################################################
+# Datasets
+################################################################################
+
+
+def make_linearly_separable_data(
+        examples_per_class: int,
+        distance_between_means: float
+):
+    """
+    Create a 2-dimensional set of points, where half the points are drawn from
+    one Gaussian and the other half are drawn from a different Gaussian
+
+    PARAMETERS
+    ----------
+    examples_per_class      An integer determining how much data we'll generate
+
+    distance_between_means  Distance between the means of the two Gaussians.
+
+    RETURNS
+    -------
+    data      A numpy array of 2 columns (dimensions) and 2*examples_per_class rows
+
+    labels    A numpy vector with 2*examples_per_class, with a +1 or -1 in each
+              element. The jth element is the label of the jth example"""
+
+    mean = [0, 0]
+    cov = [[1, 0], [0, 1]]
+
+    # negative class -1
+    negData = np.random.multivariate_normal(mean, cov, examples_per_class)
+
+    # positive class 1
+    posData = np.random.multivariate_normal(mean, cov, examples_per_class)
+    posData += distance_between_means
+
+    # make the labels
+    negL = np.ones(examples_per_class) * -1
+    posL = np.ones(examples_per_class)
+
+    # wrap it up and ship it out!
+    data = np.concatenate([posData, negData])
+    labels = np.concatenate([posL, negL])
+
+    return data, labels
+
+
+def make_XOR_data(examples_per_class: int):
+    """
+    Create a 2-dimensional set of points in the XOR pattern. Things in the
+    upper right and lower left quadrant are class 1. Things in the other two
+    quadrants are class -1.
+
+    PARAMETERS
+    ----------
+    examples_per_class      An integer determining how much data we'll generate
+
+    RETURNS
+    -------
+    data      A numpy array of 2 columns (dimensions) and 2*examples_per_class rows
+
+    labels    A numpy vector with 2*examples_per_class, with a +1 or -1 in each
+              element. The jth element is the label of the jth example"""
+
+    mean = [0, 0]
+    cov = [[1, 0], [0, 1]]
+
+    # make a circular unit Gaussian and sample from it
+    data = np.random.multivariate_normal(mean, cov, examples_per_class*2)
+
+    x = data.T[0]
+    y = data.T[1]
+
+    labels = np.sign(np.multiply(x, y))
+
+    return data, labels
+
+
+def make_center_surround_data(
+        examples_per_class: int,
+        distance_from_origin: float
+):
+    """
+    Create a 2-dimensional set of points, where half the points are drawn from
+    one Gaussian centered on the origin and the other half form a ring around
+    the first class
+
+    PARAMETERS
+    ----------
+    examples_per_class      An integer determining how much data we'll generate
+
+    distance_from_origin    All points from one of the Gaussians will have their
+                            coordinates updated to have their distance from the
+                            origin increased by this ammount. Should be
+                            non-negative.
+
+    RETURNS
+    -------
+    data      A numpy array of 2 columns (dimensions) and 2*examples_per_class rows
+
+    labels    A numpy vector with 2*examples_per_class, with a +1 or -1 in each
+              element. The jth element is the label of the jth example"""
+
+    mean = [0, 0]
+    cov = [[1, 0], [0, 1]]
+
+    # negative class -1
+    negData = np.random.multivariate_normal(mean, cov, examples_per_class)
+
+    # positive class 1
+    posData = np.random.multivariate_normal(mean, cov, examples_per_class)
+
+    # now....treat the positive class as having been drawn from phase, magnitude
+    # coordinates and manipulate the magnitude so the mean distance of the points
+    # from the origin is 4...and make sure the distribution of phase is all the way
+    # way around the circle
+    magnitude = posData.T[0, :] + distance_from_origin
+    phase = posData.T[1, :] * 2
+
+    # now go back to cartesian coordinates
+    x = magnitude * np.cos(phase)
+    y = magnitude * np.sin(phase)
+
+    # and stick it back in the array
+    posData.T[0, :] = x
+    posData.T[1, :] = y
+
+    # wrap it up and return it.
+    negL = np.ones(examples_per_class) * -1
+    posL = np.ones(examples_per_class)
+    data = np.concatenate([posData, negData])
+    labels = np.concatenate([posL, negL])
+
+    return data, labels
+
+
+def make_spiral_data(examples_per_class):
+    """
+    Create a 2-dimensional set of points in two interwoven spirals. All elements
+    in a single spiral share a label (either +1 or -1, depending on the spiral)
+
+    PARAMETERS
+    ----------
+    examples_per_class      An integer determining how much data we'll generate
+
+    RETURNS
+    -------
+    data      A numpy array of 2 columns (dimensions) and 2*examples_per_class rows
+
+    labels    A numpy vector with 2*examples_per_class, with a +1 or -1 in each
+              element. The jth element is the label of the jth example"""
+
+    theta = np.sqrt(np.random.rand(examples_per_class))*2*pi
+
+    # make points in a spiral that have some randomness
+    r_a = 2*theta + pi
+    temp = np.array([np.cos(theta)*r_a, np.sin(theta)*r_a]).T
+    negData = temp + np.random.randn(examples_per_class, 2)
+
+    # make points in a spiral offset from the first one that have some randomness
+    r_b = -2*theta - pi
+    temp = np.array([np.cos(theta)*r_b, np.sin(theta)*r_b]).T
+    posData = temp + np.random.randn(examples_per_class, 2)
+
+    # give labels to the data
+    negL = np.ones(examples_per_class) * -1
+    posL = np.ones(examples_per_class)
+
+    # return the data
+    data = np.concatenate([posData, negData])
+    labels = np.concatenate([posL, negL])
+
+    return data, labels
---
+++ code/plotting.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+################################################################################
+# Some simple plotting utilities
+################################################################################
+
+
+def plot_data(data: np.ndarray,
+              labels: np.ndarray,
+              ax: matplotlib.axes.Axes = None):
+    """
+    A helper function to plot our data sets
+
+    PARAMETERS
+    ----------
+    data      A numpy array of 2 columns (dimensions) and 2*examples_per_class rows
+
+    labels    A numpy vector with 2*examples_per_class, with a +1 or -1 in each
+              element. The jth element is the label of the jth example
+
+    ax        An optional matplotlib axis object to plot to
+    """
+
+    # require shape (n, 2)
+    assert data.ndim == 2
+    assert data.shape[-1] == 2
+
+    # plot the data
+    pos_idx = np.where(labels == 1)
+    neg_idx = np.where(labels == -1)
+
+    if ax is None:
+        ax = plt
+    ax.plot(
+        data.T[0, pos_idx],
+        data.T[1, pos_idx],
+        'r^',
+        label='positive'
+    )
+    ax.plot(
+        data.T[0, neg_idx],
+        data.T[1, neg_idx],
+        'bo',
+        label='negative'
+    )
+    ax.axis('equal')
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    plt.legend(by_label.values(), by_label.keys())
+
+    if ax is None:
+        plt.show()
+
+
+def plot_decision_surface(model=None,
+                          axis_limits=(-5, 5, -5, 5),
+                          ax: matplotlib.axes.Axes = None
+                          ):
+    """
+    Creates a grid of points, measures what a model would label each
+    point as, and uses this data to draw a region for class +1 and a region for
+    class -1.
+
+    PARAMETERS
+    ----------
+    model       A callable model that can take 2-d real-valued input and produce
+                a +1 or -1 label for each data point.
+
+    axis_limits An array-like object with 4 floats [lowest_horizontal, highest_horizontal,
+                lowest_vertical, highest_vertical]. This sets the limits over which
+                the decision surface will be caluclated and plotted.
+
+    ax          An optional matplotlib axis object to plot to
+
+    RETURNS
+    -------
+    my_contour  a matplotlib.contour.QuadContourSet with the contour
+    """
+
+    # Create a grid of points spanning the entire space displayed in the axis.
+    # This will let us draw the decision boundary later
+    xx, yy = np.meshgrid(np.arange(axis_limits[0], axis_limits[1], .05),
+                         np.arange(axis_limits[2], axis_limits[3], .05))
+    data = np.concatenate([xx.reshape([1, -1]), yy.reshape([1, -1])]).T
+
+    # Predict the class of each point in XGrid, using the classifier.
+    # This shows our regions determined by the classifier
+    if isinstance(model, nn.Module):
+        with torch.no_grad():
+            pl = model(torch.tensor(data).to(dtype=torch.float32))
+            predicted_labels = np.sign(pl.numpy())
+    else:
+        predicted_labels = model(data)
+
+    predicted_labels = predicted_labels.reshape(xx.shape)
+
+    # Put the result into a color plot
+    if ax is None:
+        ax = plt
+
+    ax.contourf(xx, yy, predicted_labels, cmap=plt.cm.Paired)
+    ax.axis('equal')
+    ax.axis('tight')
+
+    if ax is None:
+        plt.show()
